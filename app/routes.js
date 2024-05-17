ce46b277a7714f8d3abdd03dcd957e3d5f853b8c
Darren H <darren.harridence@landregistry.gov.uk>
2024-05-17 16:19:19 +0100

    new prototype wip 3x data input options

--- app/routes.js
+++ app/routes.js
@@ -25,3 +25,7 @@ router.use("/defer-time/", (req, res, next) => {
 router.use("/defer-time-dates/", (req, res, next) => {
     return require("./views/defer-time-dates/_routes")(req, res, next);
 })
+
+router.use("/defer-time-dates-v3/", (req, res, next) => {
+    return require("./views/defer-time-dates-v3/_routes")(req, res, next);
+})
