3248ced7419b958abfc0e0bb5c645f03d905d87c
Darren H <darren.harridence@landregistry.gov.uk>
2024-06-19 17:05:19 +0100

    index update work

--- app/assets/javascripts/clear-data-link.js
+++ app/assets/javascripts/clear-data-link.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function() {
     // Create the hyperlink
     const link = document.createElement('a');
     link.href = '#'; 
-    link.textContent = 'Clear data'; 
+    link.textContent = 'Clear data (popup)'; 
     link.classList.add('govuk-footer__link', 'govuk-footer__link', 'govuk-!-display-inline-block', 'govuk-!-margin-right-1', 'govuk-!-padding-top-1', 'govuk-!-padding-bottom-1');
     link.addEventListener("click", function() {// Open the popup window
         window.open('/manage-prototype/clear-data', 'targetWindow', `toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=350,height=450`);
