ad40ac82309f7d469662ea9842dae11a7a65dd9b
Darren H <darren.harridence@landregistry.gov.uk>
2024-08-30 17:27:27 +0100

    wip progress later

--- app/routes.js
+++ app/routes.js
@@ -34,6 +34,10 @@ router.use("/defer-time-dates-v3-1/", (req, res, next) => {
     return require("./views/defer-time-dates-v3/_routes")(req, res, next);
 })
 
-router.use("/quiz/", (req, res, next) => {
-    return require("./views/quiz/_routes")(req, res, next);
+router.use("/progress-later/", (req, res, next) => {
+    return require("./views/progress-later/_routes")(req, res, next);
 })
+
+//router.use("/quiz/", (req, res, next) => {
+//    return require("./views/quiz/_routes")(req, res, next);
+//}) 
