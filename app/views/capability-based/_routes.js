1383b8a56c7bc98a0c0b06e5a80009ead8c396a8
Darren H <darren.harridence@landregistry.gov.uk>
2025-10-09 16:48:07 +0100

    main pgs for categories completed

--- app/views/capability-based/_routes.js
+++ app/views/capability-based/_routes.js
@@ -6,7 +6,8 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-//flow starts ...
+//there 7 error pages to create
+
 router.post('/category-of-work-pt1', function (req, res) {
 
     const userInput = req.session.data['categoryOfWork'];
@@ -33,14 +34,98 @@ router.post('/category-of-work-pt1', function (req, res) {
               
     } else {
 
-        res.redirect("category-of-work-no-category")  //No Category 
+        res.redirect("category-of-work-list-pg")  //No Category 
 
     }
 })
 
+router.post('/category-of-work-volume', function (req, res) {
 
+    const userInput = req.session.data['categoryVolume'];
 
+    if (userInput == null) {//empty
 
+        res.redirect("category-of-work-volume-error")
+
+    } else {
+
+        res.redirect("category-of-work-list-pg")  //No Category 
+
+    }
+})
+
+router.post('/category-of-work-executive', function (req, res) {
+
+    const userInput = req.session.data['categoryExecutive'];
+    const userSubInput = req.session.data['ipsum'];
+
+    if (userInput == null) {//empty
+
+        res.redirect("category-of-work-executive-error")
+
+    } else {//one parent was selected
+
+        if (userInput == 'registerUpdateComplex' && userSubInput == null) {//empty. ie, parent and no subs selected      
+            
+            res.redirect("category-of-work-executive-subs-error")
+
+        } else if (userInput == 'registerUpdateComplex' && userSubInput == 'ipsum-b') {//parent and any of the other subs selected
+            
+            res.redirect("category-of-work-executive-tertiary")//to tertiary form pg           
+
+        } else {
+            
+            res.redirect("category-of-work-list-pg")//std return to worklist 
+
+        }
+
+    }
+})
+
+router.post('/category-of-work-executive-tertiary', function (req, res) {
+
+    const userInput = req.session.data['ipsum-b-sub'];
+
+    if (userInput == null) {//empty
+
+        res.redirect("category-of-work-executive-tertiary-error")
+
+    } else {
+
+        res.redirect("category-of-work-list-pg")  //No Category 
+
+    }
+})
+
+router.post('/category-of-work-pro-volume', function (req, res) {
+
+    const userInput = req.session.data['pro-volume'];
+
+    if (userInput == null) {//empty
+
+        res.redirect("category-of-work-pro-volume-error")
+
+    } else {
+
+        res.redirect("category-of-work-list-pg")  //No Category 
+
+    }
+})
+
+router.post('/category-of-work-pro-executive', function (req, res) {
+
+    const userInput = req.session.data['pro-executive'];
+
+    if (userInput == null) {//empty
+
+        res.redirect("category-of-work-pro-executive-error")
+
+    } else {
+
+        res.redirect("category-of-work-list-pg")  //No Category 
+
+    }
+})
 
 
 
